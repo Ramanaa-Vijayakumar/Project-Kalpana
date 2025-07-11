487750c4eca6b987747205f745c3718677f277b1
Ramanaa-Vijayakumar <160643944+Ramanaa-Vijayakumar@users.noreply.github.com>
2025-07-11 11:17:57 +0530

    Update stepper.ino

--- Task-3/Project-3-StepperMotor/stepper.ino
+++ Task-3/Project-3-StepperMotor/stepper.ino
@@ -1,18 +1,33 @@
 const int dirPin = 2;
 const int stepPin = 3;
-const int stepsPerRevolution = 200; 
+const int stepsPerRotation = 200;
 
-int rotations = 2;    
+int rotations = 2;
 bool clockwise = true;
 int speedRPM = 60;
 
 void setup(){
-  pinMode(stepPin, OUTPUT);
+  Serial.begin(9600);
+  delay(100);
+
   pinMode(dirPin, OUTPUT);
+  pinMode(stepPin, OUTPUT);
 
   digitalWrite(dirPin, clockwise ? HIGH : LOW);
 
-  int totalSteps = rotations * stepsPerRevolution;
+  Serial.println("Stepper Motor Control");
+  Serial.print("Direction: ");
+  Serial.println(clockwise ? "CounterClockwise" : "Clockwise");
+  Serial.print("Rotations: ");
+  Serial.println(rotations);
+  Serial.print("Steps per rotation: ");
+  Serial.println(stepsPerRotation);
+  Serial.print("Speed (RPM): ");
+  Serial.println(speedRPM);
+
+  int totalSteps = rotations * stepsPerRotation;
+  Serial.print("Total steps to move: ");
+  Serial.println(totalSteps);
 
   for (int i = 0; i < totalSteps; i++){
     digitalWrite(stepPin, HIGH);
@@ -20,6 +35,8 @@ void setup(){
     digitalWrite(stepPin, LOW);
     delay(10);
   }
+
+  Serial.println("Movement complete.");
 }
 
 void loop(){
